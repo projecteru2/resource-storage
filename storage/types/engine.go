a155d64c45e09399d829be96f4aeddb4268917ac
CMGS <ilskdw@gmail.com>
2023-02-01 14:58:10 +0800

    add mapstruture tag

--- storage/types/engine.go
+++ storage/types/engine.go
@@ -2,8 +2,8 @@ package types
 
 // EngineParams .
 type EngineParams struct {
-	Volumes       []string          `json:"volumes"`
-	VolumeChanged bool              `json:"volume_changed"` // indicates whether the realloc request includes new volumes
-	Storage       int64             `json:"storage"`
-	IOPSOptions   map[string]string `json:"iops_options"`
+	Volumes       []string          `json:"volumes" mapstructure:"volumes"`
+	VolumeChanged bool              `json:"volume_changed" mapstructure:"volume_changed"` // indicates whether the realloc request includes new volumes
+	Storage       int64             `json:"storage" mapstructure:"storage"`
+	IOPSOptions   map[string]string `json:"iops_options" mapstructure:"iops_options"`
 }
